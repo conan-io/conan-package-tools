8328037fc600b0a75e6beb4f0fc51621b861c16b
Lasote <lasote@gmail.com>
2016-05-09 09:14:26 +0200

    0.2.14

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.2.13",
+    version="0.2.14",
 
     description='Packaging tools for Conan C/C++ package manager',
     # long_description="An open source, decentralized package manager, to automate building and sharing of packages",
