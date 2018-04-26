c1df3b8b3a9b6fcea2ccbc987b00205cdac765ad
Luis Martinez de Bartolome <luism@jfrog.com>
2018-04-26 15:16:54 +0200

    Policy test

--- cpt/test/integration/basic_test.py
+++ cpt/test/integration/basic_test.py
@@ -152,3 +152,24 @@ class Pkg(ConanFile):
 
         self.assertTrue(found_in_export_sources)
 
+    def test_build_policy(self):
+        ci_manager = MockCIManager(build_policy="outdated")
+        conanfile = """from conans import ConanFile
+import os
+
+class Pkg(ConanFile):
+    name = "lib"
+    version = "1.2"
+    settings = "os", "compiler", "build_type", "arch"
+
+"""
+        self.save_conanfile(conanfile)
+        with tools.environment_append({"CONAN_USERNAME": "lasote"}):
+            self.packager = ConanMultiPackager(channel="mychannel",
+                                               gcc_versions=["6"],
+                                               archs=["x86", "x86_64"],
+                                               build_types=["Release"],
+                                               ci_manager=ci_manager)
+            self.packager.add_common_builds()
+            self.packager.run()
+
