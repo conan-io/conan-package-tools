24c79578a6f971263323ba333c8f197ee6070813
Luis Martinez de Bartolome <lasote@gmail.com>
2018-06-05 17:40:43 +0200

    fix test

--- cpt/test/integration/docker_test.py
+++ cpt/test/integration/docker_test.py
@@ -1,17 +1,14 @@
 import os
-import unittest
 import sys
-
 import time
-from conans.client.conan_api import ConanAPIV1
-from conans.model.ref import ConanFileReference
-
-from cpt import __version__ as version
+import unittest
 
 from conans.client import tools
+from conans.model.ref import ConanFileReference
+from cpt import __version__ as version
+from cpt.packager import ConanMultiPackager
 from cpt.test.integration.base import BaseTest, PYPI_TESTING_REPO, CONAN_UPLOAD_URL, \
     CONAN_UPLOAD_PASSWORD, CONAN_LOGIN_UPLOAD
-from cpt.packager import ConanMultiPackager
 from cpt.test.unit.utils import MockCIManager
 
 
@@ -21,7 +18,7 @@ class DockerTest(BaseTest):
     def test_docker(self):
         if not os.getenv("PYPI_PASSWORD", None):
             return 
-        self.deploy_pip()
+        # self.deploy_pip()
         ci_manager = MockCIManager()
         unique_ref = "zlib/%s" % str(time.time())
         conanfile = """from conans import ConanFile
@@ -35,6 +32,7 @@ class Pkg(ConanFile):
         the_version = version.replace("-", ".")  # Canonical name for artifactory repo
         pip = "--extra-index-url %s/simple conan-package-tools==%s " % (PYPI_TESTING_REPO, the_version)
         with tools.environment_append({"CONAN_USE_DOCKER": "1",
+                                       "CONAN_DOCKER_USE_SUDO": "1",
                                        "CONAN_PIP_PACKAGE": pip,
                                        "CONAN_LOGIN_USERNAME": CONAN_LOGIN_UPLOAD,
                                        "CONAN_USERNAME": "lasote",
@@ -54,17 +52,19 @@ class Pkg(ConanFile):
         ref = ConanFileReference.loads("%s@lasote/mychannel" % unique_ref)
 
         # Remove from remote
-        self.assertEquals(len(self.api.search_recipes(search_pattern, remote="upload_repo")), 1)
-        packages = self.api.search_packages(ref, remote="upload_repo")[0]
+        results = self.api.search_recipes(search_pattern, remote="upload_repo")["results"][0]["items"]
+        self.assertEquals(len(results), 1)
+        packages = self.api.search_packages(ref, remote="upload_repo")["results"][0]["items"][0]["packages"]
         self.assertEquals(len(packages), 2)
 
         self.api.authenticate(name=CONAN_LOGIN_UPLOAD, password=CONAN_UPLOAD_PASSWORD,
                               remote="upload_repo")
         self.api.remove(search_pattern, remote="upload_repo", force=True)
-        self.assertEquals(self.api.search_recipes(search_pattern), [])
+        self.assertEquals(self.api.search_recipes(search_pattern)["results"], [])
 
         # Try upload only when stable, shouldn't upload anything
         with tools.environment_append({"CONAN_USE_DOCKER": "1",
+                                       "CONAN_DOCKER_USE_SUDO": "1",
                                        "CONAN_PIP_PACKAGE": pip,
                                        "CONAN_LOGIN_USERNAME": CONAN_LOGIN_UPLOAD,
                                        "CONAN_USERNAME": "lasote",
@@ -81,5 +81,6 @@ class Pkg(ConanFile):
             self.packager.add_common_builds()
             self.packager.run()
 
-        self.assertEquals(len(self.api.search_recipes(search_pattern, remote="upload_repo")), 0)
+        results = self.api.search_recipes(search_pattern, remote="upload_repo")["results"]
+        self.assertEquals(len(results), 0)
         self.api.remove(search_pattern, remote="upload_repo", force=True)
