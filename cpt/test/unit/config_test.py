56d1eb5cf890996ff114a15e3ca747fb394161fe
Luis Martinez de Bartolome <lasote@gmail.com>
2018-11-02 13:34:00 +0100

    Fixed test typo

--- cpt/test/unit/config_test.py
+++ cpt/test/unit/config_test.py
@@ -17,20 +17,20 @@ class RemotesTest(unittest.TestCase):
         manager.install('https://github.com/bincrafters/conan-config.git')
         
     def test_valid_config(self):
-        manager = ConfigManager(self.api, Printer())
+        manager = ConfigManager(self.conan_api, Printer())
 
-        profiles = self.api.profile_list()
+        profiles = self.conan_api.profile_list()
         self.assertEquals(len(profiles), 0)
 
         manager.install("https://github.com/bincrafters/conan-config.git")
 
-        profiles = self.api.profile_list()
+        profiles = self.conan_api.profile_list()
         self.assertGreater(len(profiles), 3)
 
     def test_invalid_config(self):
         manager = ConfigManager(self.api, Printer())
 
-        profiles = self.api.profile_list()
+        profiles = self.conan_api.profile_list()
         self.assertEquals(len(profiles), 0)
 
         try:
