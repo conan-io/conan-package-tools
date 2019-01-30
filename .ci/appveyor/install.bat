814a980b6bab509d4f4bcceb0eea1c0ec213000f
Uilian Ries <uilianries@gmail.com>
2019-01-30 13:28:40 -0200

    Revert Appveyor changes
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .ci/appveyor/install.bat
+++ .ci/appveyor/install.bat
@@ -1,3 +1,5 @@
 SET PATH=%PYTHON%;%PYTHON%\\Scripts;%PATH%
+SET PYTHONPATH=%PYTHONPATH%;%CD%
+SET CONAN_TEST_SUITE=1
 %PYTHON%/Scripts/pip.exe install -r cpt/requirements_test.txt
 %PYTHON%/Scripts/pip.exe install -r cpt/requirements.txt
