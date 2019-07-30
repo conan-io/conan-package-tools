ae3d576d1471b9ab05e0f1f21b00728686b6494b
Luis Martinez de Bartolome <lasote@gmail.com>
2019-07-30 14:19:11 +0200

    Try fix tests

--- cpt/runner.py
+++ cpt/runner.py
@@ -45,8 +45,10 @@ class CreateRunner(object):
 
         if client_version < Version("1.12.0"):
             cache = self._conan_api._client_cache
-        else:
+        elif client_version < Version("1.18.0"):
             cache = self._conan_api._cache
+        else:
+            cache = self._conan_api.app.cache
 
         self._profile = load_profile(profile_abs_path, cache)
 
