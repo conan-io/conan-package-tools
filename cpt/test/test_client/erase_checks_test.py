f9deb22ecdc488d186274522e7d00adf4cea9d7e
Uilian Ries <uilianries@gmail.com>
2019-10-29 14:36:37 -0300

    Validate when remove is false
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- cpt/test/test_client/erase_checks_test.py
+++ cpt/test/test_client/erase_checks_test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
+from parameterized import parameterized
 
 from conans.client.tools import environment_append
 from conans.test.utils.tools import TestClient, TestServer
@@ -20,24 +21,17 @@ class Pkg(ConanFile):
         self.output.warn("OLD")
 """
 
-    new_conanfile = """from conans import ConanFile
-class Pkg(ConanFile):
-    name = "lib"
-    version = "1.0"
-    options = {"shared": [True, False], "foo": [True, False]}
-    default_options = "shared=False", "foo=True"
-
-    def build(self):
-        self.output.warn("NEW")
-"""
-
-    def test_remove_updated_packages_env_var(self):
+    @parameterized.expand([
+       ("1", "assertIn"),
+       ("0", "assertNotIn")
+    ])
+    def test_remove_updated_packages_env_var(self, remove_packages, assert_func):
         ts = TestServer(users={"user": "password"})
         tc = TestClient(servers={"default": ts}, users={"default": [("user", "password")]})
         tc.save({"conanfile.py": self.old_conanfile})
         with environment_append({"CONAN_UPLOAD": ts.fake_url, "CONAN_LOGIN_USERNAME": "user",
                                  "CONAN_PASSWORD": "password", "CONAN_USERNAME": "user",
-                                 "CONAN_REMOVE_OUTDATED_PACKAGES": "1"}):
+                                 "CONAN_REMOVE_OUTDATED_PACKAGES": remove_packages}):
             mulitpackager = get_patched_multipackager(tc, build_policy="missing",
                                                       exclude_vcvars_precommand=True)
             mulitpackager.add({}, {"shared": True})
@@ -46,9 +40,14 @@ class Pkg(ConanFile):
             self.assertIn("Uploading package 1/2", tc.out)
             self.assertIn("Uploading package 2/2", tc.out)
             self.assertIn("OLD", tc.out)
-            self.assertIn("Removing outdated packages for 'lib/1.0@user/mychannel'", tc.out)
+            getattr(self, assert_func)("Removing outdated packages for 'lib/1.0@user/testing'",
+                                       tc.out)
 
-    def test_remove_updated_packages_params(self):
+    @parameterized.expand([
+       (True, "assertIn"),
+       (False, "assertNotIn")
+    ])
+    def test_remove_updated_packages_params(self, remove_packages, assert_func):
         ts = TestServer(users={"user": "password"})
         tc = TestClient(servers={"default": ts}, users={"default": [("user", "password")]})
         tc.save({"conanfile.py": self.old_conanfile})
@@ -56,11 +55,12 @@ class Pkg(ConanFile):
                                  "CONAN_PASSWORD": "password", "CONAN_USERNAME": "user"}):
             mulitpackager = get_patched_multipackager(tc, build_policy="missing",
                                                       exclude_vcvars_precommand=True,
-                                                      remove_outdated_packages=True)
+                                                      remove_outdated_packages=remove_packages)
             mulitpackager.add({}, {"shared": True})
             mulitpackager.add({}, {"shared": False})
             mulitpackager.run()
             self.assertIn("Uploading package 1/2", tc.out)
             self.assertIn("Uploading package 2/2", tc.out)
             self.assertIn("OLD", tc.out)
-            self.assertIn("Removing outdated packages for 'lib/1.0@user/mychannel'", tc.out)
+            getattr(self, assert_func)("Removing outdated packages for 'lib/1.0@user/testing'",
+                                       tc.out)
