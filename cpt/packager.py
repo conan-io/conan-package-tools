1bdd40ee117fd22dded793a5d1cbc10dd28bfee6
Uilian Ries <uilianries@gmail.com>
2020-02-04 16:16:00 -0300

    #448 Fix pyreq loader
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- cpt/packager.py
+++ cpt/packager.py
@@ -49,6 +49,7 @@ def load_cf_class(path, conan_api):
             conan_api.create_app()
         remotes = conan_api.app.cache.registry.load_remotes()
         conan_api.app.python_requires.enable_remotes(remotes=remotes)
+        conan_api.app.pyreq_loader.enable_remotes(remotes=remotes)
         if Version(client_version) < Version("1.20.0"):
             return conan_api.app.loader.load_class(path)
         else:
