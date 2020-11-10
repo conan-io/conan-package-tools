ec92fec5cdf7d3651cf012dc12366c04ccfbf4c1
Uilian Ries <uilianries@gmail.com>
2020-11-10 12:13:21 -0300

    Allow dev branch
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- cpt/__init__.py
+++ cpt/__init__.py
@@ -6,5 +6,8 @@ NEWEST_CONAN_SUPPORTED = "1.32.000"
 def get_client_version():
     from conans.model.version import Version
     from conans import __version__ as client_version
+    from os import getenv
     # It is a mess comparing dev versions, lets assume that the -dev is the further release
+    if getenv("CONAN_TEST_SUITE", False):
+        return Version(client_version)
     return Version(client_version.replace("-dev", ""))
